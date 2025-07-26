df0ca31db93321271d3e28bb989c4ae7af238e44
vicaria <claudyvicaria@gmail.com>
2025-07-27 02:35:58 +0700

    feat(snackbar): add snackbar to layout, usage by context

--- apps/e-commerce/src/app/layout.tsx
+++ apps/e-commerce/src/app/layout.tsx
@@ -1,14 +1,27 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import './global.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Container } from '@mui/material';
+import { Alert, AlertProps, Container, Snackbar } from '@mui/material';
 
 export enum GlobalStorageKey {
   TOKEN = 'token',
   USER_ID = 'userId',
 }
+
+interface ToastOptions {
+  severity: AlertProps['severity'];
+}
+interface AppContext {
+  toast: (message: string, options?: ToastOptions) => void;
+}
+
+const AppContext = createContext<AppContext>({
+  toast: () => {
+    return;
+  },
+});
 
 const queryClient = new QueryClient();
 
@@ -17,21 +30,41 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [hasInitialized, setHasInitialized] = useState(false);
-
-  useEffect(() => {
-    setHasInitialized(true);
-  }, []);
+  const [toastProp, setToastProp] = useState<{
+    message: string;
+    options?: ToastOptions;
+  }>({ message: '' });
 
   return (
     <html lang="en">
       <body>
         <QueryClientProvider client={queryClient}>
+          <AppContext.Provider
+            value={{
+              toast: (message, options) => {
+                setToastProp({ message, options });
+              },
+            }}
+          >
+            <Snackbar
+              open={!!toastProp.message}
+              anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+              autoHideDuration={5000}
+              onClose={() => setToastProp({ message: '' })}
+            >
+              <Alert severity={toastProp.options?.severity}>
+                {toastProp.message}
+              </Alert>
+            </Snackbar>
+
           <Container component="main" sx={{ height: '100%' }}>
-            {hasInitialized && children}
+              {children}
           </Container>
+          </AppContext.Provider>
         </QueryClientProvider>
       </body>
     </html>
   );
 }
+
+export const useAppContext = () => useContext(AppContext);
